e6745f07e321dbf40d3fdc15db24509c38c8e93e
JMVCoelho <joao.vares.coelho@tecnico.ulisboa.pt>
2025-01-29 19:12:30 -0500

    subtask 1 and 2 data download

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # 11797_Project
+
+
+## Shared Task Website
+https://touche.webis.de/clef25/touche25-web/advertisement-detection.html
+
+
+
+## Data
+
+### Download the official shared task data: 
+
+```chmod +x ./download_data.sh```
+```./download_data.sh```
+
+### Download full MARCO-V2 data:
+
+TODO
+
---
+++ download_data.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+
+URL="https://zenodo.org/api/records/14680012/files-archive"
+OUTPUT_DIR="./data/subtask-2"
+OUTPUT_FILE="$OUTPUT_DIR/full.zip"
+
+mkdir -p "$OUTPUT_DIR"
+curl -L "$URL" -o "$OUTPUT_FILE"
+unzip "$OUTPUT_FILE" -d "$OUTPUT_DIR"
+rm "$OUTPUT_FILE"
+
+
+URL="https://zenodo.org/api/records/14699130/files-archive"
+OUTPUT_DIR="./data/subtask-1"
+OUTPUT_FILE="$OUTPUT_DIR/full.zip"
+
+mkdir -p "$OUTPUT_DIR"
+curl -L "$URL" -o "$OUTPUT_FILE"
+unzip "$OUTPUT_FILE" -d "$OUTPUT_DIR"
+rm "$OUTPUT_FILE"
+
+gunzip ./data/subtask-1/*.gz

50c091ab1c52d0c62e9ab9f1efc58d47cb80cd6c
kimdanny <dannykim153@gmail.com>
2025-03-10 16:34:27 -0400

    generation of data

--- synthetic_data/03_generate_data.py
+++
@@ -1,8 +0,0 @@
-"""
-Generate synthetic ads and non-ads texts
-"""
-import os
-
-CUR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-
-print(CUR_DIR_PATH)
---
+++ synthetic_data/03_generate_positive_data.py
@@ -0,0 +1,25 @@
+"""
+Generate synthetic ads (hard/easy positives)
+Use Batch API
+"""
+import os
+
+CUR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+
+DIRECT_EXPLICIT_PROMPT_BASIS = """
+
+"""
+
+DIRECT_IMPLICIT_PROMPT_BASIS = """
+
+"""
+
+INDIRECT_EXPLICIT_PROMPT_BASIS = """
+
+"""
+
+INDIRECT_IMPLICIT_PROMPT_BASIS = """
+
+"""

f5e2fb4c040abdec0ef6258e657061de98ef8c8a
JMVCoelho <joao.vares.coelho@tecnico.ulisboa.pt>
2025-03-16 22:08:50 -0400

    generate stf and dpo training data

--- rewriter/experiments/classify_answers.sh
+++ rewriter/experiments/classify_answers.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=inference_array
-#SBATCH --array=0-7
-#SBATCH --output=logs/%x-%A_%a.out
-#SBATCH -e logs/%x-%A_%a.err
+#SBATCH --job-name=inference_single
+#SBATCH --output=logs/%x-%j.out
+#SBATCH -e logs/%x-%j.err
 #SBATCH --partition=general
 #SBATCH --gres=gpu:L40S:1
 #SBATCH --cpus-per-task=1
@@ -14,13 +13,5 @@ eval "$(conda shell.bash hook)"
 conda activate vllm
 
 export HF_HOME=/data/group_data/cx_group/query_generation_data/hf_cache/
-export NCCL_P2P_DISABLE=1
 
-echo $SLURM_ARRAY_TASK_ID
-
-mkdir -p /tmp/$SLURM_ARRAY_TASK_ID/.outlines
-export OUTLINES_CACHE_DIR=/tmp/$SLURM_ARRAY_TASK_ID/.outlines
-
-df -h /tmp
-
-python generate_items.py
+python generate_dpo_sft_train_data.py
